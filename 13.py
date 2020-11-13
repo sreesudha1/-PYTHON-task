a52d559e335c983d03748a301861f358ef9423f3
sreesudha1 <72936867+sreesudha1@users.noreply.github.com>
2020-11-13 15:55:23 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,37 @@
+##to find the sum of all numbers
+##
+##numbers=[345,45,67,67,34,69,100]
+##
+##sum=0
+##
+##for var in numbers:
+##    sum=sum+var
+##
+##print("the sum is",sum)    
+
+##char=['ramesh','suresh''rajesh']
+##for i in range (len(char)):
+##    print("i like",char[i])
+
+##numbers=[23,45,677,]
+##for mem in numbers:
+##    print(mem)
+##else:
+##    print("number of items left")
+
+##to displaystudent's marks from record
+##studentname='sudha'
+##
+##marks ={'maths':56,'science'=67,'computer'=89,'statistics'=99}
+##for student in marks:
+##    if student == studentname:
+##        print(marks[student])
+##    else:
+##        print('the number will be entered')
+##
+x=['sree sudha',56,456,]
+for i in x:
+    print(i)
+
+for in in [78,9,89]:
+    print(i)
