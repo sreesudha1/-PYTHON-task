b21ffe68af50f147dcacc594f2a720a3bfd6e591
sreesudha1 <72936867+sreesudha1@users.noreply.github.com>
2020-11-22 16:53:59 +0530

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,34 @@
+def tc():
+    def ews():
+        print("welcome")
+    ews()
+tc()    
+for i in range(4):
+    print("welcome to my world")
+else:
+    print("invalid name entered")
+b=7
+while b<=7:
+    print("sree",b)
+    b+=7
+def ef():
+    def gh():
+        print("sudha")
+    gh()
+ef()    
+def sree(affected=23,noteffected=45):
+    print(affected-noteffected)
+    a = print(input("please enter the affected:"))
+    b = print(input("please enter the noteffected:"))
+a=23
+b=45
+if a>b:
+    sree(a,b)
+else:
+    sree(b,a)
+for i in range(6):
+    print("appointment letter of mnc company")
+while b<=3:
+    print("thondam",b)
+    b+=3
+
